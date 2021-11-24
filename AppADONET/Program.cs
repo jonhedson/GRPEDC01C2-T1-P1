231c2b3464e96d72f5da641af3cde966ad5bc6bb
Jonh Edson <kylix@hotmail.com>
2021-11-24 16:36:48 -0300

    Método Create Table

--- AppADONET/Program.cs
+++ AppADONET/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace AppADONET
 {
@@ -6,7 +7,48 @@ namespace AppADONET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().CreateTable();
         }
+
+        #region
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+
+            try
+            {
+                // Criar Conexão
+                con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;" +
+                    "Initial Catalog=School;" +
+                    "Integrated Security=True;" +
+                    "Connect Timeout=30;Encrypt=False;" +
+                    "TrustServerCertificate=False;" +
+                    "ApplicationIntent=ReadWrite;" +
+                    "MultiSubnetFailover=False");
+
+                SqlCommand cm = new SqlCommand("CREATE TABLE Student(id int not null," +
+                    "name varchar(100), email varchar(50), join_date date)", con);
+
+                con.Open();
+
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabela criada com sucesso");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+
+
     }
 }
