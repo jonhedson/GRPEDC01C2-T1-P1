b3fff6962dc7a266c127bee60912894024565e68
Jonh Edson <kylix@hotmail.com>
2021-11-24 17:05:46 -0300

    CRUD ADONET

--- AppADONET/Program.cs
+++ AppADONET/Program.cs
@@ -7,10 +7,13 @@ namespace AppADONET
     {
         static void Main(string[] args)
         {
-            new Program().CreateTable();
+            //new Program().CreateTable();
+            //new Program().InsertRecord();
+            //new Program().RetrieveRecord();
+            new Program().DeleteRecord();
         }
 
-        #region
+        #region CreateTable
         public void CreateTable()
         {
             SqlConnection con = null;
@@ -49,6 +52,124 @@ namespace AppADONET
         }
         #endregion
 
+        #region  Insert Records
+        public void InsertRecord()
+        {
+            SqlConnection con = null;
 
+            try
+            {
+                // Criar Conexão
+                con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;" +
+                    "Initial Catalog=School;" +
+                    "Integrated Security=True;" +
+                    "Connect Timeout=30;Encrypt=False;" +
+                    "TrustServerCertificate=False;" +
+                    "ApplicationIntent=ReadWrite;" +
+                    "MultiSubnetFailover=False");
+
+                SqlCommand cm = new SqlCommand("INSERT INTO Student (id, name, email, join_date)" +
+                    " VALUES ('101', 'Mateus Augusto', 'email@email.com',  '1/12/2021')", con);
+
+                con.Open();
+
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Registro inserido com sucesso!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+
+        #region Retrieve Records
+        public void RetrieveRecord()
+        {
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; " +
+                    "Initial Catalog=School;" +
+                    "Integrated Security=True;" +
+                    "Connect Timeout=30;Encrypt=False;" +
+                    "TrustServerCertificate=False;" +
+                    "ApplicationIntent=ReadWrite;" +
+                    "MultiSubnetFailover=False");
+
+                SqlCommand cm = new SqlCommand("SELECT * FROM Student", con);
+
+                con.Open();
+
+                SqlDataReader sdr = cm.ExecuteReader();
+
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["id"] + " " + sdr["name"] + " " + sdr["email"] + " " + sdr["join_date"]);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong.\n" + e);
+            }
+
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
+
+        #region Deleting Records
+        public void DeleteRecord()
+        {
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; " +
+                    "Initial Catalog=School;" +
+                    "Integrated Security=True;" +
+                    "Connect Timeout=30;Encrypt=False;" +
+                    "TrustServerCertificate=False;" +
+                    "ApplicationIntent=ReadWrite;" +
+                    "MultiSubnetFailover=False");
+
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("DELETE FROM Student WHERE id = '101'", con);
+
+                // Opening Connection  
+                con.Open();
+
+                // Executing the SQL query  
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Record Deleted Successfully");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong.\n" + e);
+            }
+
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+        #endregion
     }
 }
+
